c6b3fefe76193499ce923f995d894daa62007558
Rootsquare12 <3456monster@naver.com>
2023-02-26 12:20:57 +0900

    Initial Commit

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
+const http=require('http');
+const https=require('https');
 const app = require('./app');
+const fs=require('fs');
 
-app.listen(app.get('port'), () => {
-  console.log(app.get('port'), ' : Waiting Port');
-});
+/*const options = { // letsencrypt로 받은 인증서 경로를 입력
+  ca: fs.readFileSync('/opt/bitnami/apache/conf/bitnami/certs/server.crt'),
+  key: fs.readFileSync('/opt/bitnami/apache/conf/bitnami/certs/server.key'),
+  cert: fs.readFileSync('/opt/bitnami/apache/conf/bitnami/certs/server.crt')
+};*/
+
+http.createServer(app).listen(3000);
+//https.createServer(options, app).listen(443);
