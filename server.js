047bfac919b86ccfb6727af71d740ff93bd24ac7
Rootsquare12 <3456monster@naver.com>
2023-02-27 22:12:06 +0900

    Initial Commit

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const https=require('https');
 const app = require('./app');
 const fs=require('fs');
 
-const options = { // letsencrypt로 받은 인증서 경로를 입력
-  ca: fs.readFileSync('/etc/letsencrypt/live/bojquizlet-database.com/fullchain.pem'),
-  key: fs.readFileSync('/etc/letsencrypt/live/bojquizlet-database.com/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/bojquizlet-database.com/cert.pem')
-};
-
 http.createServer(app).listen(3000);
-https.createServer(options, app).listen(443);
+
+if(process.env.NODE_ENV === 'production') {
+  const options = { // letsencrypt로 받은 인증서 경로를 입력
+    ca: fs.readFileSync('/etc/letsencrypt/live/bojquizlet-database.com/fullchain.pem'),
+    key: fs.readFileSync('/etc/letsencrypt/live/bojquizlet-database.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/bojquizlet-database.com/cert.pem')
+  };
+  https.createServer(options, app).listen(443);
+}
