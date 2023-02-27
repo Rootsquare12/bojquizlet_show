4a94bb8f5fad3dfaa960aaba288740af12062c4f
Rootsquare12 <3456monster@naver.com>
2023-02-27 16:27:50 +0900

    Initial Commit

--- app.js
+++ app.js
@@ -23,12 +23,7 @@ const { update } = require('./models/user');
 dotenv.config();
 
 const app=express();
-const whitelist=["http://localhost:3000","http://localhost:5173/*","https://localhost:5173/*","https://boj-quizlet.vercel.app/*"];
-const corsOptions = {
-    origin: 'http://localhost:5173',
-    credentials: true,
-    optionsSuccessStatus: 200,
-}
+const whitelist=["http://localhost:3000","http://localhost:5173","https://localhost:5173","https://boj-quizlet.vercel.app"];
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', whitelist); 
