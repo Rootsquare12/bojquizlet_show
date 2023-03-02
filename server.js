6a8b2e67afe02472d6571eb266f532f4b5f87df5
Rootsquare12 <3456monster@naver.com>
2023-03-02 14:22:14 +0900

    HTTP

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const http=require('http');
+/*const http=require('http');
 const https=require('https');
 const app = require('./app');
 const fs=require('fs');
 
 http.createServer(app).listen(3000);
 
-/*if(process.env.NODE_ENV === 'production') {
+if(process.env.NODE_ENV === 'production') {
   const options = { // letsencrypt로 받은 인증서 경로를 입력
     ca: fs.readFileSync('/etc/letsencrypt/live/bojquizlet-database.com/fullchain.pem'),
     key: fs.readFileSync('/etc/letsencrypt/live/bojquizlet-database.com/privkey.pem'),
@@ -13,3 +13,9 @@ http.createServer(app).listen(3000);
   };
   https.createServer(options, app).listen(443);
 }*/
+
+const app = require('./app');
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기중');
+});
