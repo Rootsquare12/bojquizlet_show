01c9cbb3b732ddb26b46469de727b200ea7f7d87
Rootsquare12 <3456monster@naver.com>
2023-03-06 10:35:35 +0900

    LogIn

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,10 @@
 const logger=require('../logger');
 const {Op}=require('sequelize');
 const bcrypt = require('bcrypt');
+const jwt=require('jsonwebtoken');
 const User = require('../models/user');
 
-exports.newUser = async (req, res, next) => {//새로운 회원 가입하기
+exports.newUser=async (req, res, next) => {//새로운 회원 가입하기
   try
   {//먼저 이메일부터 중복을 확인한다.
     const email=req.body.email;
@@ -33,6 +34,42 @@ exports.newUser = async (req, res, next) => {//새로운 회원 가입하기
         });
         res.send("Successfully Created User Data.");
       }
+    }
+  } catch (error) {
+    logger.error(error);
+  }
+}
+
+exports.loginProcess=async (req, res, next) => {//새로운 회원 가입하기
+  try
+  {//먼저 이메일부터 중복을 확인한다.
+    const nick=req.body.nickname;
+    const password=req.body.password;
+    const exUser=await User.findOne({where: {nickname:nick}});
+    if(exUser)
+    {
+      const result=await bcrypt.compare(password,exUser.password);
+      if(result)
+      {//아이디와 비밀번호가 모두 맞는 경우 토큰을 만들어 전송한다.
+        const key=process.env.JWT_SECRET;
+        console.log(key);
+        const token=jwt.sign({
+          type:"JWT",
+          nickname: nick,
+        },key,{
+          expiresIn:"30m",//토큰 유효기간
+          issuer:"rootsquare",//토큰 발행자
+        })
+        res.status(200).send(token);
+      }
+      else
+      {//비밀번호가 틀린 경우
+        res.status(404).send("Wrong User ID or password.");
+      }
+    }
+    else
+    {//아이디가 잘못된 경우
+      res.status(404).send("Wrong User ID or password.");
     }
   } catch (error) {
     logger.error(error);
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const express=require('express');
-const {newUser} = require('../controllers/auth');
+const {newUser,loginProcess} = require('../controllers/auth');
 
 const router=express.Router();
 
-router.post('/join',newUser);//유저 id의 정보
+router.post('/join',newUser);//회원가입하기
+router.post('/login',loginProcess);//로그인하기
 
 module.exports=router;
