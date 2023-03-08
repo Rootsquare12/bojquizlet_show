abd3c65f6c25679830f015cda012177127cd7bd8
Rootsquare12 <3456monster@naver.com>
2023-03-08 13:13:46 +0900

    API code change

--- routes/solutions.js
+++ routes/solutions.js
@@ -28,10 +28,10 @@ const solution_upload=multer();
 
 router.post('/img',image_upload.single('img'),uploadPictures);//그림 파일 올리기
 
-router.get('/:id',verifyToken,renderSolutions);//id 문제의 해설들 가져오기
-router.post('/:id/write',verifyToken,solution_upload.none(),writeSolution);//새로운 해설 쓰기
-router.put('/:id/update',verifyToken,solution_upload.none(),updateSolution);//특정 해설 수정하기
-router.get('/:id/:user',renderCertainSolution);//id 문제의 특정 해설 가져오기
+router.get('/find/:id',verifyToken,renderSolutions);//id 문제의 해설들 가져오기
+router.get('/find/:id/:user',renderCertainSolution);//id 문제의 특정 해설 가져오기
+router.post('/write/:id',verifyToken,solution_upload.none(),writeSolution);//새로운 해설 쓰기
+router.put('/update/:id',verifyToken,solution_upload.none(),updateSolution);//특정 해설 수정하기
 //router.delete('/:id/write/:user/delete',deleteSolution);//해설 삭제하기
 
 module.exports=router;
