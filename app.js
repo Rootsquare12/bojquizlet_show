2cb4b131e843ca99d5c8a48f427cc10b558db7ed
Rootsquare12 <3456monster@naver.com>
2023-03-17 22:19:15 +0900

    Final

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.static(path.join(__dirname,'public')));
-app.use('/img',express.static(path.join(__dirname,'uploads')));
+app.use('/uploads',express.static(path.join(__dirname,'uploads')));
 
 app.set('port', process.env.PORT || 3000);
 
