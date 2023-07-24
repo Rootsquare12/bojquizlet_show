ca2506eb1bf1f396b14cee0dfd49a8e4e740f38d
Rootsquare12 <3456monster@naver.com>
2023-07-24 21:54:14 +0900

    Server Update

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,7 +10,7 @@ exports.newUser=async (req, res, next) => {//새로운 회원 가입하기
     const email=req.body.email;
     const exEmail=await User.findOne({where: {email: email}});
     if (exEmail) {//이미 존재하는 이메일일 경우
-      res.send("E-Mail Already Exists.");
+      res.status(404).send("E-Mail Already Exists.");
     }
     else
     {
@@ -18,7 +18,7 @@ exports.newUser=async (req, res, next) => {//새로운 회원 가입하기
       const exNick=await User.findOne({where: {nickname: nick}});
       if(exNick)
       {//이미 존재하는 닉네임일 경우
-        res.send("Nickname Already Exists.");
+        res.status(404).send("Nickname Already Exists.");
       }
       else
       {//모두 만족하면 데이터베이스에 등록한다.
@@ -29,7 +29,7 @@ exports.newUser=async (req, res, next) => {//새로운 회원 가입하기
           nickname:nick,
           password:hashed_password,
         });
-        res.send("Successfully Created User Data.");
+        res.status(200).send("Successfully Created User Data.");
       }
     }
   } catch (error) {
